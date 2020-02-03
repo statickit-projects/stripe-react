b801fe00f614162fea87580c87698a4c61fa463a
Derrick Reimer <derrickreimer@gmail.com>
2020-02-03 13:45:34 -0600

    Install analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
+import React, { useEffect } from 'react';
 import Head from 'next/head';
+import Router from 'next/router';
 import { StaticKitProvider } from '@statickit/react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import * as Fathom from 'fathom-client';
 import '../style.css';
 
 const stripePromise = loadStripe('pk_test_AEGjmWosrdHvOvnujk0cNHjQ');
 
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
+
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      Fathom.load();
+      Fathom.setSiteId('ZFEWBXJZ');
+      Fathom.trackPageview();
+    }
+  }, []);
+
   return (
     <StaticKitProvider site={process.env.siteId}>
       <Elements stripe={stripePromise}>
